d26a341a71681f61953d81659590caceec809933
EFO1048 <60328427+EFO1048@users.noreply.github.com>
2023-09-21 10:29:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
   print("Goodbye")
+  print("This is another addition")
   # This is another test
   x = 5
   y = 6
-  z = x + y
+  a = 7
+  z = x + a
+  print("This is an addition")
 
 if __name__ == "__main__":
   main()
+  
