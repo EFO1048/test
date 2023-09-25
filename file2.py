83b98ed6c5250a0e7c96e272661d12993f81b0f6
EFO1048 <60328427+EFO1048@users.noreply.github.com>
2023-09-25 11:54:47 -0400

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,11 @@
+def getHeaders():
+
+  old_headers = {
+      "Authorization": "Bearer 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"
+  }
+
+  headers = {
+      "Authorization": "Bearer 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"
+  }
+
+  return headers
